f68cc5f0d58faa1901e29c38cce92b7dc25322b7
Washington <cardosowashington2003@gmail.com>
2024-02-03 17:10:35 -0300

    Finish for the moment

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+    var scrollButton = document.getElementById("scrollButton");
+  
+    if (scrollButton) {
+      scrollButton.addEventListener("click", function() {
+        var projectsSection = document.getElementById("projects");
+  
+        if (projectsSection) {
+          projectsSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    }
+});
